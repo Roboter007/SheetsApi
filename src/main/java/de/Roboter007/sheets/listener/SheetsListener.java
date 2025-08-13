84b0a9816690619b6ecd2f540a2ee7c95b3c9d9f
Roboter007 <r07.schikora@gmail.com>
2025-08-13 23:49:08 +0200

    Quick Implementation

--- src/main/java/de/Roboter007/sheets/listener/SheetsListener.java
+++ src/main/java/de/Roboter007/sheets/listener/SheetsListener.java
@@ -1,5 +1,6 @@
 package de.Roboter007.sheets.listener;
 
+import de.Roboter007.sheets.SheetsApi;
 import de.Roboter007.sheets.SheetsPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,7 +10,8 @@ public record SheetsListener(SheetsPlugin plugin) implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
+        SheetsApi.getLogger().info("Checking for Missing Player Data...");
+        plugin.getPlayerDataManager().checkForMissingPlayerData(event.getPlayer().getUniqueId());
     }
 
 
